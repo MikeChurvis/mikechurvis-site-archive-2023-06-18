698774edabc6f2a5849d6a43f22bc6a68d1a23f4
mikechurvis <michael@productivityenhancement>
2021-10-21 19:27:43 -0400

    Deploy script builds site and injects .nojekyll

--- StaticSite/deploy.sh
+++ StaticSite/deploy.sh
@@ -1,3 +1,7 @@
+cd StaticSite/portfolio
+npm run build
+cd ../..
+touch StaticSite/portfolio/dist/.nojekyll
 git subtree split --prefix StaticSite/portfolio/dist -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
