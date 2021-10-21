4e56b57d8a9b627d9f9a9a1478ca80ba99be72e6
mikechurvis <michael@productivityenhancement>
2021-10-21 19:32:35 -0400

    Deployment script *actually commits* the build before pushing it to the deployment branch

--- StaticSite/deploy.sh
+++ StaticSite/deploy.sh
@@ -1,7 +1,8 @@
 cd StaticSite/portfolio
 npm run build
+touch dist/.nojekyll
+git commit -am "Deployment Build ($(date))"
 cd ../..
-touch StaticSite/portfolio/dist/.nojekyll
 git subtree split --prefix StaticSite/portfolio/dist -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
