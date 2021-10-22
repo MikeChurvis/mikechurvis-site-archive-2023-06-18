00dda6cf117f1ae0642715eba07625f1515a3df5
mikechurvis <michael@productivityenhancement>
2021-10-22 14:31:32 -0400

    Deployment Build (Fri, Oct 22, 2021  2:31:32 PM)

--- StaticSite/deploy.sh
+++ StaticSite/deploy.sh
@@ -1,11 +1,11 @@
 cd StaticSite/portfolio
 npm run build
-touch dist/_astro/.nojekyll
+touch dist/.nojekyll
+cd ../..
 git add -A
 git status
 git commit -m "Deployment Build ($(date))"
 git push
-cd ../..
 git subtree split --prefix StaticSite/portfolio/dist -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
