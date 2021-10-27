a42ac47600b4d840026ae6f57a495b5dacd147b2
mikechurvis <michael@productivityenhancement>
2021-10-27 17:53:24 -0400

    Deployment Build (Wed, Oct 27, 2021  5:53:24 PM)

---
+++ StaticSite/portfolio/src/components/stackdemo/StackBlock.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="outer" :class="dashed ? 'dashed-border' : ''">
+        {{ icon }} {{ title }}
+        <slot></slot>
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: "StackBlock",
+    props: {
+        title: String,
+        icon: String,
+        vertical: Boolean,
+        dashed: Boolean,
+    }
+})
+</script>
+
+
+<style lang="scss">
+    .outer {
+        border: 1px solid black;
+        
+        &.dashed-border {
+            border: 1px dashed black;
+        }
+    }
+</style>
