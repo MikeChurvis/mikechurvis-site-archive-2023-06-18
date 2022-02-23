0e220ec679a3807c10708d936344654ff13f112d
mikechurvis <mikechurvis@gmail.com>
2022-02-23 12:21:58 -0500

    Deployment Build (Wed, Feb 23, 2022 12:21:58 PM)

--- frontend/portfolio/deploy.sh
+++ frontend/portfolio/deploy.sh
@@ -1,7 +1,13 @@
-cd frontend/portfolio
+#!/usr/bin/env bash
+
+CALL_DIRECTORY=$(pwd)
+SCRIPT_DIRECTORY=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/../.."
+
+cd "${SCRIPT_DIRECTORY}"
 npm run build
-touch dist/.nojekyll
-cd ../..
+
+cd "${PROJECT_ROOT_DIRECTORY}"
 git add -A
 git status
 git commit -m "Deployment Build ($(date))"
@@ -9,3 +15,5 @@ git push
 git subtree split --prefix frontend/portfolio/dist -b gh-pages
 git push -f origin gh-pages:gh-pages
 git branch -D gh-pages
+
+cd "${CALL_DIRECTORY}"
