b8480c13ff1f206910095dc28f3e982e99d3c498
mikechurvis <mikechurvis@gmail.com>
2022-03-21 18:58:32 -0400

    Prepared Nginx for mikechurvis.com domain

--- backend/portfolio/portfolio/urls.py
+++ backend/portfolio/portfolio/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
-    path('api/contact/', include('ContactForm.urls')),
+    path('admin/', admin.site.urls),
+    path('contact/', include('ContactForm.urls')),
 ]
