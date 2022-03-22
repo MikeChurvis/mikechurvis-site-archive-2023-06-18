5fc2bc341bf0b2d20240de9fd829fdd5e3dfdd9e
mikechurvis <mikechurvis@gmail.com>
2022-03-22 11:27:59 -0400

    Debug nginx.conf

--- backend/portfolio/portfolio/settings.py
+++ backend/portfolio/portfolio/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    env('DJANGO_STATIC_ROOT', os.path.join(BASE_DIR, 'static')),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
