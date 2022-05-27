7ec0fa5986fe88e698e8e6e584eac814f4ceab2e
Mike Churvis <mikechurvis@gmail.com>
2022-05-27 17:47:28 -0400

    Rename MailingList app to Emailer

--- api-v2/Emailer/apps.py
+++ api-v2/Emailer/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MailinglistConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MailingList'
+    name = 'Emailer'
--- api-v2/PortfolioAPI/settings.py
+++ api-v2/PortfolioAPI/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'ContactForm',
-    'MailingList',
+    'Emailer',
 ]
 
 MIDDLEWARE = [
